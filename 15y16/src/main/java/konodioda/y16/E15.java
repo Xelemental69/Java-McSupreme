5b582fc257b16dfc810b5600b5b0274a288087a8
Xelemental <xelemental864@gmail.com>
2021-10-14 11:11:46 +0200

    Ejercicios Introducción 14 y 15

---
+++ 15y16/src/main/java/konodioda/y16/E15.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package konodioda.y16;
+
+/**
+ *
+ * @author fco-j
+ */
+public class E15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        boolean adivina = ((97 == 'a') && true);
+		System.out.println(adivina);
+        int a = 'a';
+		System.out.println(a);
+
+	//int pi = 3.14; No funciona porque int no acepta decimales
+		//System.out.println(pi);
+                  
+	double pii = 3.14; //Hay que cambiar la coma por punto
+                System.out.println(pii);
+
+	boolean adivinam = (1==4);
+                System.out.println(adivina);
+
+
+
+        
+    }
+    
+}
