5276858d66bb945fc82c85e0c544efea0ca8cd3b
Xelemental <xelemental864@gmail.com>
2021-10-15 14:43:05 +0200

    emoSidoEngañaos

---
+++ BancoFranJ/src/main/java/tetimofuerte/bancofranj/BancoEmoSidoEnganiaos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tetimofuerte.bancofranj;
+
+/**
+ *
+ * @author fco-j
+ */
+public class BancoEmoSidoEnganiaos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
