7bd169d979c73df905f258458796b471ec6814bf
Xelemental <xelemental864@gmail.com>
2021-10-19 13:09:14 +0200

    EmoSido Engañaos

--- BancoFranJ/src/main/java/tetimofuerte/bancofranj/BancoEmoSidoEnganiaos.java
+++ BancoFranJ/src/main/java/tetimofuerte/bancofranj/BancoEmoSidoEnganiaos.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tetimofuerte.bancofranj;
+
 import java.util.*;
+
 /**
  *
  * @author fco-j
@@ -21,52 +23,53 @@ public class BancoEmoSidoEnganiaos {
         double maxEuros, sueldoBruto, cantEulos;
         final double FACTOR_TETIMO = 5.85;
         //Empezamos a pedir los datos, comenzando por la edad y el sueldo bruto:
-        System.out.println("Bienvenido al banco TeTimo, cliente.\n" + 
-                "Introduzca su edad: ");
+        System.out.println("Bienvenido al banco TeTimo, cliente.\n"
+                + "Introduzca su edad: ");
         edad = entry.nextInt();
         System.out.println("Introduce el sueldo bruto que posee: ");
         sueldoBruto = entry.nextDouble();
         //Comprobamos si entra en el rango de edad deseado
-        if(edad<65&&edad>=18){
-        //Si su edad es apta, seguimos, si no, saltamos hacia el else nº1
-        
+        if (edad < 65 && edad >= 18) {
+            //Si su edad es apta, seguimos, si no, saltamos hacia el else nº1
+
             //Pedimos cantidad para el préstamo:
             System.out.println("¿Cuánta cantidad de dinero solicita usted?");
             cantEulos = entry.nextDouble();
             //Comprobamos el máximo de euros posible para prestar:
             maxEuros = sueldoBruto * FACTOR_TETIMO;
-            
+
             //Si se mantiene en el rango, seguimos, si no, al else nº2
-            if(cantEulos<=maxEuros){
+            if (cantEulos <= maxEuros) {
                 //Pedimos los años hasta la devolución
                 System.out.println("¿En cuántos años estima la devolución?");
                 anosHipoteca = entry.nextInt();
                 //Continuamos por el if si la duración no es excesiva
                 //No obstante, si dura más de 30 años o si sumado a la edad
                 //superan los 75, saltamos al else nº3
-                if((anosHipoteca<30 && anosHipoteca<5) 
-                        && (anosHipoteca + edad)>75){
+                if ((anosHipoteca < 30 && anosHipoteca < 5)
+                        && (anosHipoteca + edad) > 75) {
                     //Una vez todos los requisitos son cumplidos, se considera
-                    //al cliente apto para el club Emosido Engañaos de
+                    //al cliente apto para el club EmoSido Engañaos de
                     //TeTimo(Fuerte)^TM
-                    System.out.println("Estupendo, es usted apto para hacer el " 
+                    System.out.println("Estupendo, es usted apto para hacer el "
                             + "engaño, digo, préstamo de " + cantEulos);
-                    
-                }else{//Else nº3. Damos error por ser demasiada duración.
+
+                } else {//Else nº3. Damos error por ser demasiada duración.
                     //Y tras ello, termina el programa
-                    System.out.println("Perdone, pero no podemos mantener un " 
-                    + "préstamo por tanto tiempo.");
+                    System.out.println("Perdone, pero no podemos mantener un "
+                            + "préstamo por tanto tiempo.");
                 }
-                
-            }else{//Else nº2. Damos error por el exceso de cantidad y terminamos
-                System.out.println("Lo sentimos, pero no podemos darle " 
-                + "tanta cantidad.");
+
+            } else {//Else nº2. Damos error por el exceso de cantidad y terminamos
+                System.out.println("Lo sentimos, pero no podemos darle "
+                        + "tanta cantidad.");
             }
-            
-        }else //Else nº1. Si su edad no es apta, salta error y termina esto
+
+        } else //Else nº1. Si su edad no es apta, salta error y termina esto
+        {
             System.out.println("No posee la edad necesaria para el préstamo.");
-        
-        
+        }
+
     }
-    
+
 }
